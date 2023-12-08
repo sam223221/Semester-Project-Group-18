56dbea1f793957d1f42db1fa950bce4948d601f1
kamils555 <ksuchodolski@vp.pl>
2023-12-08 18:47:07 +0100

    minor changes

--- WorldOfZuul/WorldOfZuul/WorldOfZuul/obj/Debug/net6.0/WorldOfZuul.AssemblyInfo.cs
+++ WorldOfZuul/WorldOfZuul/WorldOfZuul/obj/Debug/net6.0/WorldOfZuul.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46771ede79c16136740e296a244bd336f613ecb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
